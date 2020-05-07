8f6da245b5375ff946421c9491833698bbfe091e
SharonTLitye <sharonthandekasiko@gmail.com>
2020-05-07 17:28:37 +0200

    footer and navbar link

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,65 @@
 //<!--- Start Navigation -->
-
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
 //<!--- End Navigation -->
-
 
 //<!--- Typewriter-Landing Page Section -->
 
-var app = document.getElementById('app');
+var app = document.getElementById("app");
 
 var typewriter = new Typewriter(app, {
-    loop: true
+	loop: true,
 });
 
-typewriter.typeString('User interface disi')
+typewriter
+	.typeString("User interface disi")
 	.pauseFor(1000)
 	.deleteChars(3)
-	.typeString('esign')
+	.typeString("esign")
 	.pauseFor(2500)
 	.deleteAll()
-	.typeString('User experience')
+	.typeString("User experience")
 	.pauseFor(2500)
 	.deleteAll()
-	.typeString('Web development')
-    .pauseFor(2500)
+	.typeString("Web development")
+	.pauseFor(2500)
 	.deleteAll()
-	.typeString('User interface design')
-    .pauseFor(2500)
+	.typeString("User interface design")
+	.pauseFor(2500)
 	.deleteAll()
-	.typeString('Mobile development')
-    .pauseFor(2500)
+	.typeString("Mobile development")
+	.pauseFor(2500)
 	.deleteAll()
-	.typeString('Software development')
-    .pauseFor(2500)
-    
-    .start();
+	.typeString("Software development")
+	.pauseFor(2500)
+
+	.start();
 //<!--- End Landing Page Section -->
-
-
-
-
-
-
-
-
 
 //<!--- Start Carousel Section -->
 
