56645bfdd2c403fc56bd6757d6269f288f7ee91f
SharonTLitye <sharonthandekasiko@gmail.com>
2020-05-11 02:16:50 +0200

    cart functionality

--- js/scripts.js
+++ js/scripts.js
@@ -113,3 +113,66 @@ $("#testimonials-carousel").owlCarousel({
 });
 
 // <!--- End Testimonials Section Carousel -->
+
+// <!--- Start Cart Section -->
+let carts = document.querySelectorAll('.add-to-enquiry');
+
+let products = [
+	{
+		name: 'Full Stack Web Developer',
+		tag: 'fullstackwebdeveloper',
+		price: 26000,
+		inCart: 0
+	},
+	{
+		name: 'Front End Web Developer',
+		tag: 'frontendwebdeveloper',
+		price: 26000,
+		inCart: 0
+	},
+	{
+		name: 'Java Spring Boot Developer',
+		tag: 'javaspringbootdeveloper',
+		price: 26000,
+		inCart: 0
+	}]
+
+
+for (let i=0; i < carts.length; i++) {
+	carts[i].addEventListener('click', () => {
+		cartNumbers(products[i]);
+	})
+}
+
+function onLoadCartNumbers(){
+	let productNumbers = localStorage.getItem('cartNumbers');
+
+	if(productNumbers){
+		document.querySelector('.cart span').textContent = productNumbers;
+	}
+
+}
+
+function cartNumbers(product) {
+	let productNumbers = localStorage.getItem('cartNumbers');
+
+	productNumbers = parseInt(productNumbers);
+
+	if(productNumbers){
+		localStorage.setItem('cartNumbers',productNumbers + 1);
+		document.querySelector('.cart span').textContent = productNumbers + 1;
+	}else{
+		localStorage.setItem('cartNumbers', 1);
+		document.querySelector('.cart span').textContent = 1;
+	}	
+
+	seItems(product);
+}
+
+function setItems(product){
+	
+}
+
+
+onLoadCartNumbers();
+// <!--- End Cart Section -->
