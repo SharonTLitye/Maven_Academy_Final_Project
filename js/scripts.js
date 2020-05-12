2e7688224e4881f41d5e2dabf8208fd44274619a
SharonTLitye <sharonthandekasiko@gmail.com>
2020-05-12 18:36:05 +0200

    cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,34 @@
 //<!--- Start Navigation -->
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
+	var z, i, elmnt, file, xhttp;
+	/* Loop through a collection of all HTML elements: */
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+		/*search for elements with a certain atrribute:*/
+		file = elmnt.getAttribute("w3-include-html");
+		if (file) {
+			/* Make an HTTP request using the attribute value as the file name: */
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function () {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						elmnt.innerHTML = this.responseText;
+					}
+					if (this.status == 404) {
+						elmnt.innerHTML = "Page not found.";
+					}
+					/* Remove the attribute, and call this function once more: */
+					elmnt.removeAttribute("w3-include-html");
+					includeHTML();
+				}
+			};
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			/* Exit the function: */
+			return;
+		}
+	}
 }
 //<!--- End Navigation -->
 
@@ -115,64 +119,75 @@ $("#testimonials-carousel").owlCarousel({
 // <!--- End Testimonials Section Carousel -->
 
 // <!--- Start Cart Section -->
-let carts = document.querySelectorAll('.add-to-enquiry');
+let carts = document.querySelectorAll(".add-to-enquiry");
 
 let products = [
 	{
-		name: 'Full Stack Web Developer',
-		tag: 'fullstackwebdeveloper',
+		name: "Full Stack Web Developer",
+		tag: "fullstackwebdeveloper",
 		price: 26000,
-		inCart: 0
+		inCart: 0,
 	},
 	{
-		name: 'Front End Web Developer',
-		tag: 'frontendwebdeveloper',
+		name: "Front End Web Developer",
+		tag: "frontendwebdeveloper",
 		price: 26000,
-		inCart: 0
+		inCart: 0,
 	},
 	{
-		name: 'Java Spring Boot Developer',
-		tag: 'javaspringbootdeveloper',
+		name: "Java Spring Boot Developer",
+		tag: "javaspringbootdeveloper",
 		price: 26000,
-		inCart: 0
-	}]
+		inCart: 0,
+	},
+];
 
-
-for (let i=0; i < carts.length; i++) {
-	carts[i].addEventListener('click', () => {
-		cartNumbers(products[i]);
-	})
+for (let i = 0; i < carts.length; i++) {
+	carts[i].addEventListener("click", () => {
+		cartNumbers();
+	});
 }
 
-function onLoadCartNumbers(){
-	let productNumbers = localStorage.getItem('cartNumbers');
-
-	if(productNumbers){
-		document.querySelector('.cart span').textContent = productNumbers;
-	}
-
-}
-
-function cartNumbers(product) {
-	let productNumbers = localStorage.getItem('cartNumbers');
+function cartNumbers() {
+	let productNumbers = localStorage.getItem("cartNumbers");
 
 	productNumbers = parseInt(productNumbers);
 
-	if(productNumbers){
-		localStorage.setItem('cartNumbers',productNumbers + 1);
-		document.querySelector('.cart span').textContent = productNumbers + 1;
-	}else{
-		localStorage.setItem('cartNumbers', 1);
-		document.querySelector('.cart span').textContent = 1;
-	}	
+	if (productNumbers) {
+		localStorage.setItem("cartNumbers", productNumbers + 1);
+		document.querySelector(".cart span").textContent = productNumbers + 1;
+	} else {
+		localStorage.setItem("cartNumbers", 1);
+		document.querySelector(".cart span").textContent = 1;
+	}
 
-	seItems(product);
+	setItems(productNumbers);
 }
 
-function setItems(product){
-	
+function onLoadCartNumbers() {
+	let productNumbers = localStorage.getItem("cartNumbers");
+	productNumbers = parseInt(productNumbers);
+
+	if (productNumbers) {
+		setItems(productNumbers);
+		document.querySelector(".cart span").textContent = productNumbers;
+	}
 }
 
+function setItems(product) {
+	let cartItems = localStorage.getItem("productsInCart");
+	cartItems = JSON.parse(cartItems);
+	console.log("My cartItems are", cartItems);
 
+	if (cartItems != null) {
+		cartItems[product.tag].inCart += 1;
+	}
+	product.inCart = 1;
+	let CartItems = {
+		[product.tag]: product,
+	};
+
+	localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
 onLoadCartNumbers();
 // <!--- End Cart Section -->
